b2606adcbf5aa42d10ec6c36c8cafdf09d09c0f3
kevinmenden <kevin.menden@t-online.de>
2019-03-15 19:24:17 +0100

    First tests, building some code

--- rl_car_v0.1.py
+++ rl_car_v0.1.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from collections import namedtuple
 from itertools import count
 from PIL import Image
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -17,3 +16,196 @@ import torchvision.transforms as T
 
 # Create environment
 env = gym.make("MountainCarContinuous-v0")
+
+class ReplayMemory(object):
+    """
+    Replay Memory Class
+    """
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+    """
+    The Q approximating network
+    Architecture from DQN paper
+    """
+    def __init__(self, h, w):
+        super(DQN, self).__init__()
+
+        self.n_actions = 1
+        self.gamma = 0.9
+        self.eps_final = 0.0001
+        self.eps_start = 0.1
+        self.n_iter = 2000000
+        self.replay_memory_size = 10000
+        self.batch_size = 32
+
+        self.conv1 = nn.Conv2d(4, 32, 8, 4)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.fc1 = nn.Linear(3136, 512)
+        self.fc2 = nn.Linear(512, self.n_actions)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+
+# Resize function to make the picture smaller and in tensor format
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+
+def get_screen():
+    """
+    Render a screen, transpose, normalize and resize it
+    :return:
+    """
+    screen = env.render(mode='rgb_array').transpose(2, 0, 1)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    screen = resize(screen)
+    return screen
+
+def select_action(state):
+    """
+    Select an action given the current state
+    :param state: difference between two consecutive screens
+    :return: action (list with one float)
+    """
+    a = 1.0
+    return a
+
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+
+init_screen = get_screen()
+_, _, screen_height, screen_width = init_screen.shape
+
+policy_net = DQN(screen_height, screen_width)
+target_net = DQN(screen_height, screen_width)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(10000)
+
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(2)]], device=device, dtype=torch.long)
+
+
+episode_durations = []
+
+
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+
+# store rewards of episodes
+episode_rewards = []
+
+
+## Run Everything
+num_episodes = 50
+for i_episode in range(num_episodes):
+    # Initialize the environment and state
+    env.reset()
+    # Get current and last screen, calculate state
+    last_screen = get_screen()
+    current_screen = get_screen()
+    state = current_screen - last_screen
+
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        _, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward])
+
+        # Observe new state
+        last_screen = current_screen
+        current_screen = get_screen()
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the target network)
+        optimize_model()
+        if done:
+            episode_rewards.append(reward)
+            break
+    # Update the target network, copying all weights and biases in DQN
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
+
+print('Complete')
+env.render()
+env.close()
+plt.ioff()
+plt.show()
---
+++ testing.py
@@ -0,0 +1,75 @@
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+
+
+# Create environment
+env = gym.make("MountainCarContinuous-v0")
+print(env.action_space)
+print(env.observation_space)
+
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+def get_screen():
+    screen = env.render(mode='rgb_array').transpose(2, 0, 1)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    screen = resize(screen).unsqueeze(0)
+    return screen
+
+
+class DQN(nn.Module):
+    """
+    The Q approximating network
+    Architecture from DQN paper
+    """
+    def __init__(self):
+        super(DQN, self).__init__()
+
+        self.n_actions = 1
+        self.gamma = 0.9
+        self.eps_final = 0.0001
+        self.eps_start = 0.1
+        self.n_iter = 2000000
+        self.replay_memory_size = 10000
+        self.batch_size = 32
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = nn.Linear(512, self.n_actions)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size()[0], -1)
+        print(x.shape)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+net = DQN()
+screen = get_screen()
+env.close()
+
+net.forward(screen)
